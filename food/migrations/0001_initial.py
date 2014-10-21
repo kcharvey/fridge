ea0d2103999da791b905a3a7eb57de251cfbb944
Kevin Harvey <kevin.harvey@smilecareclub.com>
2014-10-21 17:42:05 -0500

    refactor to base app, add recipe model

--- food/migrations/0001_initial.py
+++ food/migrations/0001_initial.py
@@ -15,10 +15,25 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=30)),
+                ('slug', models.SlugField(blank=True)),
                 ('quantity', models.IntegerField()),
                 ('unit', models.CharField(max_length=30)),
             ],
             options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Recipe',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30)),
+                ('slug', models.SlugField(blank=True)),
+                ('ingredients', models.ManyToManyField(to='food.Food')),
+            ],
+            options={
+                'abstract': False,
             },
             bases=(models.Model,),
         ),
--- food/models.py
+++ food/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+from django.utils.text import slugify
 
-class Food(models.Model):
+
+class BaseModel(models.Model):
 
     name = models.CharField(max_length=30)
+    slug = models.SlugField(blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(unicode(self.name))
+        super(BaseModel, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Food(BaseModel):
+
     quantity = models.IntegerField()
     unit = models.CharField(max_length=30)
+
+
+class Recipe(BaseModel):
+
+    ingredients = models.ManyToManyField(Food)
+
