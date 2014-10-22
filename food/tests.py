16ff84fc8c05136c47717ee9c1a86f850aaf1864
Kevin Harvey <kevin.harvey@smilecareclub.com>
2014-10-22 01:53:35 -0500

    sample unit test

--- food/tests.py
+++ food/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Food
+
+class FoodTestCase(APITestCase):
+
+    def setUp(self):
+        food1 = Food.objects.create(
+            name = 'Leftover Lasagna',
+            quantity = 2,
+            unit = 'pieces'
+        )
+
+    def test_get_foods(self):
+        response = self.client.get('/food/', format='json')
+        self.assertEqual(response.data[0]['name'], u'Leftover Lasagna')
