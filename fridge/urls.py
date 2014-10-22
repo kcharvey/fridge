80b2ead3f6531e914e24cf5278b9a78f6a34ba2e
Kevin Harvey <kevin.harvey@smilecareclub.com>
2014-10-22 10:18:11 -0500

    add static to urls

--- fridge/urls.py
+++ fridge/urls.py
@@ -21,3 +21,10 @@ urlpatterns = patterns('',
     # Django admin
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 
+	'django.views.static.serve', 
+	{'document_root': settings.STATIC_ROOT}),
+    )
