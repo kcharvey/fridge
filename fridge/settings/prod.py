99f99445e4a8813de88eca58b58f0ff3f68161a5
Kevin Harvey <kevin.harvey@smilecareclub.com>
2014-10-22 02:12:35 -0500

    remove BASE_DIR from prod settings

--- fridge/settings/prod.py
+++ fridge/settings/prod.py
@@ -13,8 +13,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#import os
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
