00ade7d9ae8641e6f32ced7ed63b217f9b939a2d
Kevin Harvey <kevin.harvey@smilecareclub.com>
2014-10-22 10:40:02 -0500

    hmmmmm...

--- fridge/wsgi.py
+++ fridge/wsgi.py
@@ -13,4 +13,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fridge.settings.prod")
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
