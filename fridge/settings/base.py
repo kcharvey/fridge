cd6aae87775a1c761a61cca73f2b4fe7ea479a46
Kevin Harvey <kevin.harvey@smilecareclub.com>
2014-10-22 11:12:09 -0500

    toss static_dirs setting

--- fridge/settings/base.py
+++ fridge/settings/base.py
@@ -81,9 +81,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
